58518c48fea980652e7b52765bc31bccb28bfd23
Nikimad <PybePoiD@gmail.com>
2023-11-24 16:30:03 +0300

    Change src/app/layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,11 @@
-import ReduxProvider from "@/components/ReduxProvider";
-
 export const metadata = {
   title: "test-app",
 };
 
-const RootLayout = ({ children }) => {
-  return (
-    <ReduxProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ReduxProvider>
-  );
-};
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
 
 export default RootLayout;
