f90d8e105f6605444145c5c20605310a2393b531
Nikimad <PybePoiD@gmail.com>
2023-11-28 13:42:55 +0300

    Change next.config.js: add proxying

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const defineNextConfig = (config) => config;
 
-export default defineNextConfig({});
+export default defineNextConfig({
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://interns-test-fe.snp.agency/api/v1/:path*",
+      },
+    ];
+  },
+});
