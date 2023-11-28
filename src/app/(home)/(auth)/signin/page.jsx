e1ae87e5339335b3315b9d4337507812ef1897d4
Nikimad <PybePoiD@gmail.com>
2023-11-28 13:52:46 +0300

    Change src/app/(home)/(auth)/signin/page.jsx

--- src/app/(home)/(auth)/signin/page.jsx
+++ src/app/(home)/(auth)/signin/page.jsx
@@ -1,3 +1,46 @@
-const SigninPage = () => "Signin page";
+"use client";
+
+import useAction from "@/hooks/useAction";
+import useForm from "@/hooks/useForm";
+import { authActions } from "@/models/auth";
+
+const SigninPage = () => {
+  const [data, handlers] = useForm({
+    username: "",
+    password: "",
+  });
+
+  const signin = useAction(authActions.signin);
+
+  const handleSubmit = (e) => {
+    handlers.onSubmit(e);
+    signin(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign in</h2>
+      <label>
+        Username
+        <input
+          type="text"
+          name="username"
+          value={data.username}
+          onChange={handlers.onChange}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="text"
+          name="password"
+          value={data.password}
+          onChange={handlers.onChange}
+        />
+      </label>
+      <button type="submit">Sign in</button>
+    </form>
+  );
+};
 
 export default SigninPage;
